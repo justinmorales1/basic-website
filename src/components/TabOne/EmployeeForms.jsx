c232d0dbf72f26bb50f60481fce967f073d758c7
justinmorales1 <jm.em0210@gmail.com>
2021-02-24 22:37:02 -0600

    Adding employee forms

--- src/components/TabOne/EmployeeForms.jsx
+++ src/components/TabOne/EmployeeForms.jsx
@@ -1,8 +1,49 @@
+import { Field, reduxForm } from 'redux-form'
 
-export default () => {
+const EmployeeForms  = (props) => {
+    const { handleSubmit} = props;
     return (
-        <div>
-            Employee Forms
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="container">
+            <div>
+                <label>First Name</label>
+                <Field
+                    name="username"
+                    type="text"
+                    component="input"
+                    label="Username"
+                    placeholder="First Name"
+                    style={{marginBottom: '20px'}}
+                />
+            </div>
+            <div>
+                <label>Email</label>
+                <Field
+                    name="email"
+                    type="text"
+                    component="input"
+                    label="Email"
+                    placeholder="Email"
+                    style={{marginBottom: '20px'}}/>
+            </div>
+            <div>
+                <label>Age</label>
+                <Field
+                    name="age"
+                    type="text"
+                    component="input"
+                    label="Age"
+                    placeholder="Age"
+                    style={{marginBottom: '20px'}}/>
+            </div>
+                <button type="submit">
+                    Submit
+                </button>
+            </div>
+        </form>
     )
 }
+
+export default reduxForm({
+    form: 'employeeForm' // a unique identifier for this form
+})(EmployeeForms)
