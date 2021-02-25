5a385e061615a8df6d586c5ffcd7adbefae7792f
justinmorales1 <jm.em0210@gmail.com>
2021-02-24 20:44:04 -0600

    component updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Employees from './components/TabTwo/MemberForms';
-import LaborQueries from './components/TabOne/EmployeeForms';
+import LandingPage from './components/LandingPage'
+import MemberForms from './components/TabTwo/MemberForms';
+import EmployeeForms from './components/TabOne/EmployeeForms';
 import ThirdPartyForms from './components/TabThree/ThirdPartyForms';
 
 
@@ -10,9 +12,12 @@ class App extends React.Component {
         return (
             <div>
                 <Header/>
-                <Employees/>
-                <LaborQueries/>
-                <ThirdPartyForms/>
+                <BrowserRouter>
+                    <LandingPage exact path='/'/>
+                    <EmployeeForms exact path='/employees-forms'/>
+                    <MemberForms exact path='/member-forms'/>
+                    <ThirdPartyForms exact path='/third-party-forms'/>
+                </BrowserRouter >
             </div>
         )
     }
